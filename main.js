5a685dd46209b3403c8c4eb7c844cca175affd1c
NazarPolit <144369818+NazarPolit@users.noreply.github.com>
2023-10-06 00:11:34 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function analyzeAndWriteToFile(inputFilePath, outputFilePath) {
+  fs.readFile(inputFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Помилка читання файлу JSON:', err);
+      return;
+    }
+
+    try {
+      const jsonData = JSON.parse(data);
+      const outputData = [];
+
+      for (const item of jsonData) {
+        if (item.parent === "BS3_BanksLiab") {
+          const result = `${item.txt}:${item.value}`;
+          outputData.push(result);
+        }
+      }
+
+      fs.writeFile(outputFilePath, outputData.join('\n'), 'utf8', (err) => {
+        if (err) {
+          console.error('Помилка запису в файл:', err);
+        } else {
+          console.log('Результати аналізу записані у файл output.txt');
+        }
+      });
+    } catch (err) {
+      console.error('Помилка при розборі JSON:', err);
+    }
+  });
+}
+
+analyzeAndWriteToFile('data.json', 'output.txt');
